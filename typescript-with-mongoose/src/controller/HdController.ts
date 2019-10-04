ed8739519108570b6ac3bb2218e26004d7eb7bcd
Jason Ferguson <fergusonjason@gmail.com>
2019-10-04 05:33:38 -0400

    implemented all methods in HdController

--- typescript-with-mongoose/src/controller/HdController.ts
+++ typescript-with-mongoose/src/controller/HdController.ts
@@ -4,6 +4,7 @@ import { IHd } from "../model/HD";
 import { BaseController } from "./BaseController";
 import { HdRepository } from "../repository/HdRepository";
 import { IInitializesRoutes } from "./InitializesRoutes";
+import { IPagedDataResponse } from "../model/PagedDataResponse";
 
 export class HdController extends BaseController<IHd> implements IInitializesRoutes {
 
@@ -26,53 +27,84 @@ export class HdController extends BaseController<IHd> implements IInitializesRou
 
         logger.debug("Initializing routers for hd");
 
+        this.router.get(`${this.prefix}`, this.get);
         this.router.get(`${this.prefix}/count`, this.count);
-        this.router.get(`${this.prefix}/get`, this.get);
         this.router.get(`${this.prefix}/getAll`, this.getAll);
         this.router.get(`${this.prefix}/page`, this.page);
-        this.router.get(`${this.prefix}/getByHd`, this.getByHdNumber);
+    }
+
+    public get = async (req : Request, res : Response) : Promise<void> => {
+
+        logger.debug(`HDController: entered get(), id: ${req.query.id}`);
+
+        const id : number = req.query.id;
+        if (!id) {
+            res.status(400).send("No id provided");
+            return;
+        }
+
+        try {
+            let result : IHd | null | object = await this.hdRepository.getById(id);
+            if (result == null) {
+                result = {};
+            }
+            res.json(result);
+        } catch (err) {
+            res.status(500).send(`Error occurred: ${err}`);
+        }
+    }
+
+    public getAll = async (req : Request, res : Response) : Promise<void> => {
+
+        logger.debug(`HDController: entered getAll()`);
+
+        try {
+            let result : IHd[] | null = await this.hdRepository.getAll();
+            if (result == null) {
+                result = [];
+            }
+
+            res.json(result);
+        } catch (err) {
+            res.status(500).send(`Error occurred: ${err}`);
+        }
     }
 
     public count = async (req : Request, res : Response) : Promise<void> => {
 
         logger.debug("HdController: entered count()");
 
-        const count : number = await this.hdRepository.count();
-
-        const result : object = {count : count};
-
-        res.send(result);
-
-    }
-
-    public getByHdNumber = async (req : Request, res : Response) : Promise<void> => {
-
-        logger.debug("HDController: Entered getByHdNumber");
-
-        const hdNumber : number = parseInt(req.query.hdNumber, 10);
-
         try {
-            const result : IHd | null = await this.hdRepository.getByHdNumber(hdNumber);
-            res.json(result);
+            const count : number = await this.hdRepository.count();
+            const result : object = {count : count};
+            res.send(result);
         } catch (err) {
-            res.send(`Error occurred: ${err}`);
+            res.status(500).send(`Error: ${err}`);
         }
 
     }
 
-    public get = async (req : Request, res : Response) : Promise<void> => {
+    public page = async (req : Request, res : Response) : Promise<void> => {
 
-    }
+        logger.debug(`HDController: entered page(), start: ${req.query.start}, end: ${req.query.stop}`);
 
-    public getAll = (req : Request, res : Response) : void => {
+        const start : number = parseInt(req.query.start, 10);
+        const stop : number = parseInt(req.query.stop, 10);
 
-    }
+        try {
+            const count : number = await this.hdRepository.count();
+            const items : IHd[] = await this.hdRepository.getPage(start, stop);
+            const result : IPagedDataResponse<IHd[]> = {
+                result : items,
+                start: start,
+                stop: stop,
+                totalRecords: count
+            };
 
-    public page = (req : Request, res : Response) : void => {
-
-    }
-
-    private initStore = () : void => {
+            res.json(result);
+        } catch (err) {
+            res.status(500).send(`Error: ${err}`);
+        }
 
     }
 }
--- typescript-with-mongoose/src/repository/HdRepository.ts
+++ typescript-with-mongoose/src/repository/HdRepository.ts
@@ -9,20 +9,18 @@ export class HdRepository extends BaseRepository<IHd> {
         super(HD_MODEL);
     }
 
-    public getByHdNumber = async (hdNumber : number) : Promise<IHd | null> => {
-
-        logger.debug("Entered getByHdNumber");
-
-        const result : IHd | null = await this._model.findOne({HD: hdNumber});
-
-        return result;
-    }
-
     public getById = async (id : number): Promise<IHd | null> => {
 
         const result : IHd | null = await this._model.findOne({HD: id});
 
         return result;
+    }
+
+    public getAll = async () : Promise<IHd[] | null> => {
+
+        logger.debug("getAll() is not implemented. Use getPage()");
+
+        return null;
     }
 
     public getPage = async (start : number, stop : number) : Promise<IHd[]> => {
