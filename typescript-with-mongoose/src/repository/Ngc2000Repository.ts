d18304a8907d62de6cf3fc1e2b8dd141ebb67ca3
Jason Ferguson <fergusonjason@gmail.com>
2019-10-05 12:47:25 -0400

    fix tslint warning

--- typescript-with-mongoose/src/repository/Ngc2000Repository.ts
+++ typescript-with-mongoose/src/repository/Ngc2000Repository.ts
@@ -2,7 +2,7 @@ import { INgc2000, NGC2000_MODEL } from "../model/Ngc2000";
 import { BaseRepository } from "./BaseRepository";
 import { logger } from "../util/winston";
 
-export class Ngc2000Repository extends BaseRepository<INgc2000>{
+export class Ngc2000Repository extends BaseRepository<INgc2000> {
 
     constructor() {
         super(NGC2000_MODEL);
