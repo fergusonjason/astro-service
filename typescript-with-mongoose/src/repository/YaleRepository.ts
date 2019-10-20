07e667ee535dc14ccb2d49a2fb8c70c652c69e1e
Jason Ferguson <fergusonjason@gmail.com>
2019-10-20 15:07:47 -0400

    cleanup dead code

--- typescript-with-mongoose/src/repository/YaleRepository.ts
+++ typescript-with-mongoose/src/repository/YaleRepository.ts
@@ -16,41 +16,4 @@ export class YaleRepository extends BaseRepository<IYale> {
         return await this._model.findOne({HR : id});
     }
 
-    // public getPage = async (start : number, pageSize : number, field? : string, sortDir? : number,
-    //                         filterOp? : string, filterVal? : string | number) : Promise<IYale[]> => {
-
-    //     logger.debug(`YaleRepository: entered getPage(): start: ${start}, pageSize: ${pageSize},
-    //         field: ${field}, sortDir: ${sortDir}`);
-
-    //     if (!field) {
-    //         logger.debug("No field specified, using HR");
-    //         field = "HR";
-    //     }
-
-    //     if (!sortDir) {
-    //         sortDir = 1;
-    //     }
-
-    //     const findObj : IQueryObj = {};
-    //     const sortObj : IQueryObj = {};
-    //     const mongoOp : string = "$" + filterOp;
-    //     if (filterOp && filterVal) {
-
-    //         sortObj[field] = sortDir;
-
-    //         switch (filterOp) {
-    //             case "startsWith" :
-    //                 const regexp : RegExp = new RegExp("^" + filterVal);
-    //                 findObj[field] = { $regex : regexp};
-    //                 break;
-    //             default:
-    //                 findObj[field] = { [mongoOp] : filterVal };
-    //         }
-    //     }
-
-    //     // logger.debug(`Findobj: ${JSON.stringify(findObj)}, sortobj: ${JSON.stringify(sortObj)}`);
-
-    //     return await this._model.find(findObj).sort(sortObj).skip(start)
-    //         .limit(pageSize).exec();
-    // }
 }
